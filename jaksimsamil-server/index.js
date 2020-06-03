2a3979dff015698a57118abe7c2d1de1d0924ac4
YongWooSong <syw5141@khu.ac.kr>
2020-06-03 20:48:45 +0900

    Init MongoDB

--- jaksimsamil-server/index.js
+++ jaksimsamil-server/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 const app = express();
 require("dotenv").config();
-
+const { SERVER_PORT, MONGO_URL } = process.env;
 app.use(
   morgan("[:date[iso]] :method :status :url :response-time(ms) :user-agent")
 );
@@ -10,6 +11,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", require("./api"));
 
-app.listen(process.env.SERVER_PORT, () => {
+mongoose
+  .connect(MONGO_URL, { useNewUrlParser: true, useFindAndModify: false })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+app.listen(SERVER_PORT, () => {
   console.log("Server is running on port", process.env.SERVER_PORT);
 });
