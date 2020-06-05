6edae0b0567124cc5764e055219d3598e509fbb0
YongWooSong <syw5141@khu.ac.kr>
2020-06-05 11:31:17 +0900

    Fixed untracked files

--- jaksimsamil-server/src/api/profile/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.post("/solved:id");
-router.get("/solvednum:id");
-router.get("recommendps:id");
-
-module.exports = router;
--- jaksimsamil-server/src/api/user/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.post("/");
-router.delete("/:id");
-router.get("/:id");
-router.get("");
-
-module.exports = router;
