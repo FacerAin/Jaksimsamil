72d1e01177580ba8dc5f103b723c9a3d50ffb8ab
YongWooSong <syw5141@khu.ac.kr>
2020-08-18 16:38:14 +0900

    Update Challenge Container

--- jaksimsamil-page/src/pages/ChallengePage.js
+++ jaksimsamil-page/src/pages/ChallengePage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import HeaderContainer from '../containers/common/HeaderContainer';
-
+import ChallengeContainer from '../containers/challenge/ChallengeContainer';
 const ChallengePage = () => {
   return (
     <div>
       <HeaderContainer />
+      <ChallengeContainer />
     </div>
   );
 };
