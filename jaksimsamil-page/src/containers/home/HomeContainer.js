0a64d87ac33df5b79ec5ab2e97cf9d3d318c608b
YongWooSong <syw5141@khu.ac.kr>
2020-08-25 21:56:40 +0900

    Update Heatmap calendar

--- jaksimsamil-page/src/containers/home/HomeContainer.js
+++ jaksimsamil-page/src/containers/home/HomeContainer.js
@@ -1,14 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import HomeForm from '../../components/home/HomeForm';
 import { getPROFILE, initializeProfile } from '../../modules/profile';
 const HomeContainer = ({ history }) => {
   const dispatch = useDispatch();
+  const [HMArr, setHMArr] = useState([]);
   const { user, profile } = useSelector(({ user, profile }) => ({
     user: user.user,
     profile: profile,
   }));
+
+  const makeHeatmapValues = (PSdata) => {
+    let obj_keys = Object.keys(PSdata);
+    let result = [];
+    for (let i = 0; i < obj_keys.length; i++) {
+      result.push({
+        date:
+          //2019-10-15
+          obj_keys[i].slice(0, 4) +
+          '-' +
+          obj_keys[i].slice(4, 6) +
+          '-' +
+          obj_keys[i].slice(6, 8),
+        count: PSdata[obj_keys[i]].length,
+      });
+    }
+    return result;
+  };
 
   useEffect(() => {
     if (!user) {
@@ -23,7 +42,9 @@ const HomeContainer = ({ history }) => {
     }
   }, [dispatch, user, history]);
   useEffect(() => {
-    console.log(profile);
+    if (profile.solvedBJ_date) {
+      setHMArr(makeHeatmapValues(profile.solvedBJ_date.solvedBJbyDATE));
+    }
   }, [profile]);
   useEffect(() => {
     if (user) {
@@ -31,6 +52,12 @@ const HomeContainer = ({ history }) => {
       dispatch(getPROFILE({ username }));
     }
   }, [dispatch, user]);
-  return <HomeForm PSdata={profile.solvedBJ_date} goalNum={profile.goalNum} />;
+  return (
+    <HomeForm
+      PSdata={profile.solvedBJ_date}
+      HMArr={HMArr}
+      goalNum={profile.goalNum}
+    />
+  );
 };
 export default withRouter(HomeContainer);
